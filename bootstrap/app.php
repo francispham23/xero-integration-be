15f145b93d3c78efe03411ffe4f5589ff0b386ce
Francis Pham <francis.pham.ca@gmail.com>
2025-02-16 16:30:49 -0800

    add xero package

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Webfox\Xero\Exceptions\OAuthException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Handle when the user clicks cancel on the Xero authorization screen
+        $exceptions->render(function (OAuthException $e, Request $request) {
+            return redirect('/my/xero/connect/page')->with('errorMessage', $e->getMessage());
+        });
     })->create();
+
